cf2d886ebab64044b2f570f829532b37aed92cad
IskanderN <iskander.sych@gmail.com>
2020-07-13 01:44:29 +0300

    [doc] : docstings for dataset

--- dataset.py
+++ dataset.py
@@ -6,6 +6,11 @@ from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 
 
 class SequenceDataset(Dataset):
+    """
+    Dataset, which samples sequences with length N from every tensor from tensors
+    :param tensors: list of tensor with same length
+    :param N: length of sampled tensors
+    """
     def __init__(self, *tensors: t.List[torch.Tensor], N: int = 1024):
         super().__init__()
         assert all([tensor.shape[0] == tensors[0].shape[0] for tensor in tensors])
@@ -21,6 +26,14 @@ class SequenceDataset(Dataset):
 
 
 def make_data_loader(*tensors, N, batch_size, num_batches) -> torch.utils.data.DataLoader:
+    """
+    Creates DataLoader, which samples num_batches batches of sequences with shape (batch_size, N) from tensors
+    :param tensors: tensors to sample batches from
+    :param N: length of sampled subsequences
+    :param batch_size: batch size
+    :param num_batches: amount of batches to sample
+    :return: DataLoader
+    """
     assert tensors[0].shape[0] % N == 0, "length of tensors has to be dividable by N!"
     M = tensors[0].shape[0] // N
     dataset = SequenceDataset(*tensors, N=N)
