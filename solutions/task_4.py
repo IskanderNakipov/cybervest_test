5e7589195f6f322a964b01b33bfe60f5c3220cee
IskanderN <iskander.sych@gmail.com>
2020-07-14 02:05:13 +0300

    [ref] : save model after training

--- solutions/task_4.py
+++ solutions/task_4.py
@@ -1,3 +1,5 @@
+import json
+
 import torch
 
 from model import Model, train
@@ -6,18 +8,23 @@ from helpers import parse_args, timer
 
 
 if __name__ == '__main__':
-    model = Model()
     args = parse_args()
+    config = {'hidden_size': args.hidden_size, 'num_layers': args.num_layers}
+    model = Model(**config)
+    print(model)
     filename = "task_4.log"
     x_msg = f"X generation with N = {args.N} and M = {args.M}"
     X = timer(generate_x, filename, x_msg)(args.M, args.N)
     y_msg = "Finding optimums for X"
     YMin, YMax = timer(find_min_max, filename, y_msg)(X, args.T, args.k)
 
-    X_val = generate_x(args.M, args.N)
+    X_val = generate_x(1, args.val_size)
     YMin_val, YMax_val = find_min_max(X_val, args.T, args.k)[:args.N]
     X, X_val, YMin, YMin_val, YMax, YMax_val = [torch.from_numpy(x) for x in [X, X_val, YMin, YMin_val, YMax, YMax_val]]
     training_message = "Training model"
     timer(train, filename, training_message)(model, X, X_val, YMin, YMin_val, YMax, YMax_val, args.N, args.M,
                                              args.epochs, args.lr, args.num_batches, args.batch_size, args.device)
+    torch.save(model.state_dict(), 'model.pkl')
+    with open('config.json', 'w') as f:
+        json.dump(config, f, ensure_ascii=False, indent=4)
 
