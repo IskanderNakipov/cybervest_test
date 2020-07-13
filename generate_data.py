caa95e07f5a60743c5aecdfa35713e3c8be983fe
IskanderN <iskander.sych@gmail.com>
2020-07-13 23:06:14 +0300

    [fix] : replaced placeholder for min/max search with real function

--- generate_data.py
+++ generate_data.py
@@ -11,12 +11,39 @@ def generate_x(M: int, N: int = 1024):
 
 def find_min_max(X, T=3, k=10):
     """Finds local optimums for X"""
-    #TODO: replace placeholder with real function
     min_dist = T*np.std(X[:-k] - X[k:])
-    YMin = np.zeros_like(X)
+    max_index = X.argmax()
+
+    indexes = np.zeros_like(X)
+    indexes[max_index] += 1
+    _find_min_max(X, min_dist, max_index, indexes)
+    _find_min_max(X, min_dist, max_index, indexes, step=-1)
     YMax = np.zeros_like(X)
-    YMin[list(range(0, X.shape[0], 200))] += 1
-    YMax[list(range(2, X.shape[0], 200))] += 1
+    YMax[indexes == 1] += 1
+    YMin = np.zeros_like(X)
+    YMin[indexes == -1] += 1
     return YMin, YMax
 
 
+def _find_min_max(X, min_dist, start, indexes, step=1):
+    index = start
+    end = X.shape[0] if step > 0 else -1
+    candidate = None
+    on_max = 1
+    for i in range(start, end, step):
+        if candidate is None and (X[index] - X[i]) * on_max >= min_dist:
+            candidate = i
+            continue
+        if candidate is not None:
+            if X[i] * on_max <= X[candidate] * on_max:
+                candidate = i
+            elif (X[i] - X[candidate]) * on_max >= min_dist:
+                indexes[candidate] -= on_max
+                index = candidate
+                candidate = i
+                on_max *= -1
+    if candidate is not None:
+        indexes[candidate] -= on_max
+    else:
+        indexes[end] -= on_max
+    return indexes
