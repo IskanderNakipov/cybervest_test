2c88ea327f5e554c8bfa3b3c892a1262605770f5
IskanderN <iskander.sych@gmail.com>
2020-07-13 20:36:21 +0300

    [ref] : refactoring of model.py

--- model.py
+++ model.py
@@ -17,8 +17,9 @@ def parse_args():
     parser.add_argument('--M', type=int, help="Parameter M from task 1", required=True)
     parser.add_argument('--T', type=int, help="Parameter T from task 1", default=3)
     parser.add_argument('--k', type=int, help="Parameter k from task 1", default=10)
-    parser.add_argument('--num_batches', '-nb', help='Amount of batches to sample', default=100)
+    parser.add_argument('--num_batches', '-nb', help='Amount of batches to sample every epoch', default=100)
     parser.add_argument('--batch_size', '-B', help='Batch size', default=16)
+    parser.add_argument('--lr', help='Learning rate', default=1e-3, type=float)
     return parser.parse_args()
 
 
@@ -38,6 +39,21 @@ class Model(nn.Module):
         return self.classifier(X)
 
 
+def _unpack_data(x: t.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> t.Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Unpacks data to suitable for neural network form
+    :param x: tuple [input, YMin, YMax]
+    :return: training data and labels
+    """
+    X, YMin, YMax = x
+    placeholder = torch.ones_like(YMin) * 0.5
+    Y = torch.cat([placeholder.unsqueeze(-1),
+                   YMin.unsqueeze(-1),
+                   YMax.unsqueeze(-1)], dim=-1)
+    Y = Y.argmax(dim=-1)
+    return X, Y
+
+
 def _train_step(model: Model, opt: optim.Optimizer,
                 x: t.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> t.Tuple[torch.Tensor, float, float]:
     """
@@ -47,12 +63,8 @@ def _train_step(model: Model, opt: optim.Optimizer,
     :param x: tuple [input, YMin, YMax]
     :return: loss, f1-score for minimums anf f1-score for maximums
     """
-    X, YMin, YMax = x
-    placeholder = torch.ones_like(YMin) * 0.5
-    Y = torch.cat([placeholder.unsqueeze(-1),
-                   YMin.unsqueeze(-1),
-                   YMax.unsqueeze(-1)], dim=-1)
-    Y = Y.argmax(dim=-1)
+    X, Y = _unpack_data(x)
+    YMin, YMax = x[1:]
     pred = model(X.unsqueeze(-1).float())
     loss = nn.functional.cross_entropy(flatten(pred), Y.flatten())
     loss.backward()
@@ -63,28 +75,49 @@ def _train_step(model: Model, opt: optim.Optimizer,
     return loss, min_f1, max_f1
 
 
-def _eval(model, X_val, YMin_val, YMax_val, device):
-    X_val, YMin_val, YMax_val = [torch.from_numpy(x).to(device) for x in [X_val, YMin_val, YMax_val]]
-    placeholder = torch.ones_like(YMin_val) * 0.5
-    Y = torch.cat([placeholder.unsqueeze(-1),
-                   YMin_val.unsqueeze(-1),
-                   YMax_val.unsqueeze(-1)], dim=-1)
-    Y = Y.argmax(dim=-1)
-    model.eval()
+def _eval(model: Model, x: t.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> t.Tuple[torch.Tensor, float, float]:
+    """
+    Evaluates model
+    :param model: model to eval
+    :param x: tuple [input, YMin, YMax]
+    :return: loss, f1-score for minimums anf f1-score for maximums
+    """
+    X, Y = _unpack_data(x)
+    YMin, YMax = x[1:]
     with torch.no_grad():
-        pred = model(X_val.unsqueeze(-1).unsqueeze(0).float())
+        pred = model(X.unsqueeze(-1).unsqueeze(0).float())
         loss = nn.functional.cross_entropy(flatten(pred), Y.flatten())
-        min_f1 = f1(flatten(pred), YMin_val.flatten())
-        max_f1 = f1(flatten(pred), YMax_val.flatten())
+        min_f1 = f1(flatten(pred), YMin.flatten())
+        max_f1 = f1(flatten(pred), YMax.flatten())
     return loss, min_f1, max_f1
 
 
-def train(model: Model, X, X_val, YMin, YMin_val, YMax, YMax_val,
-          N=1024, M=1000, num_epoch=10,
-          epoch_size=1000, batch_size=32, device='cuda:0'):
+def train(model: Model, X: torch.Tensor, X_val: torch.Tensor,
+          YMin: torch.Tensor, YMin_val: torch.Tensor,
+          YMax: torch.Tensor, YMax_val: torch.Tensor,
+          N: int = 1024, M: int = 1000, num_epoch: int = 10, lr: float = 1e-3,
+          epoch_size: int = 1000, batch_size: int = 32, device: str = 'cuda:0') -> Model:
+    """
+    Trains model
+    :param model: model to train
+    :param X: Training data
+    :param X_val: validation data
+    :param YMin: Training labels for minimums
+    :param YMin_val: validation labels for minimums
+    :param YMax: Training labels for maximums
+    :param YMax_val: validation labels for maximums
+    :param N: length of subsequences in data
+    :param M: amount of subsequences in data
+    :param num_epoch: amount of epochs to train model
+    :param lr: learning rate
+    :param epoch_size: amount of batches to feed every epoch
+    :param batch_size: batch size
+    :param device: device
+    :return: trained model
+    """
     assert X.shape[0] == N*M
     model.to(device)
-    opt = optim.Adam(model.parameters())  #TODO: add lr
+    opt = optim.Adam(model.parameters(), lr=lr)
     for epoch in range(num_epoch):
         with tqdm(total=epoch_size, desc=f'epoch {epoch}') as tq:
             model.train()
@@ -94,7 +127,8 @@ def train(model: Model, X, X_val, YMin, YMin_val, YMax, YMax_val,
                 tq.set_postfix(loss=loss.item())
                 tq.update()
             model.eval()
-        _eval(model, X_val, YMin_val, YMax_val, device)
+        _eval(model, [x_.to(device) for x_ in [X_val, YMin_val, YMax_val]])
+    return model
 
 
 def main():
