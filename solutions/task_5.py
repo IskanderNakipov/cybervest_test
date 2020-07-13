b28d455030ccd0a2617215a5744175240883d64b
IskanderN <iskander.sych@gmail.com>
2020-07-14 02:12:44 +0300

    [ref] : load model with load method

--- solutions/task_5.py
+++ solutions/task_5.py
@@ -1,20 +1,21 @@
 import numpy as np
+import torch
 from matplotlib import pyplot as plt
 
-from helpers import parse_args, timer
-
 from generate_data import generate_x, find_min_max
-from visualisation import visualise_x, visualise_probas
-
+from helpers import parse_args, timer
+from model import Model
+from visualisation import visualise_probas
 
 if __name__ == '__main__':
-    filename = 'task_4.log'
+    filename = 'task_5.log'
     args = parse_args()
     x_msg = f"X generation with N = {args.N} and M = {args.M}"
     X = timer(generate_x, filename, x_msg)(args.M, args.N)
     y_msg = "Finding optimums for X"
     YMin, YMax = timer(find_min_max, filename, y_msg)(X, args.T, args.k)
-    probas = np.random.rand(X.shape[0], 3)  # TODO: replace with real model
+    model = Model.load(args.model_path)
+    probas = model.predict_proba(torch.from_numpy(X).unsqueeze(0).unsqueeze(-1).float()).numpy()
     for _ in range(args.amount_graphs):
         start = np.random.randint(0, args.N * (args.M - 1))
         visualise_probas(X, probas, start, args.N, YMin, YMax)
