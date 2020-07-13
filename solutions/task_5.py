428004c31edf652a361dd3fd013d6f7ae81bd46b
IskanderN <iskander.sych@gmail.com>
2020-07-14 02:53:01 +0300

    [add] : plot confusion matrix and evaluate model

--- solutions/task_5.py
+++ solutions/task_5.py
@@ -1,11 +1,12 @@
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
 
 from generate_data import generate_x, find_min_max
 from helpers import parse_args, timer
 from model import Model
-from visualisation import visualise_probas
+from visualisation import visualise_probas, plot_confusion_matrix
 
 if __name__ == '__main__':
     filename = 'task_5.log'
@@ -20,4 +21,13 @@ if __name__ == '__main__':
         start = np.random.randint(0, args.N * (args.M - 1))
         visualise_probas(X, probas, start, args.N, YMin, YMax)
         plt.legend()
-        plt.show()
+    plt.show()
+    with torch.no_grad():
+        pred = model(torch.from_numpy(X).unsqueeze(0).unsqueeze(-1).float()).numpy()
+        pred = pred.squeeze(0).argmax(axis=-1)
+        Y = np.zeros_like(X)
+        Y[YMin == 1] += 1
+        Y[YMax == 1] += 2
+        cm = confusion_matrix(Y, pred)
+        print(classification_report(Y, pred))
+    plot_confusion_matrix(cm, target_names=['None', 'Min', 'Max'])
